bf5cc3bc22a1a25ea53a1cbe812002f405a204eb
inbarBenjo <154405262+inbarBenjo@users.noreply.github.com>
2023-12-28 21:38:35 +0200

    test15

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -63,7 +63,7 @@ int fourChild=0;
         int max1 = Math.max(twoChild,threeCchild);
         int mostCommonChild = Math.max(max1,fourChild); 
 
-         System.out.println("Average: " + average + " children to get at least one of each gender");
+         System.out.println("Average: " + average + " children to get at least one of each gender.");
          System.out.println("Number of families with 2 children: " + twoChild); 
          System.out.println("Number of families with 3 children: " + threeCchild); 
          System.out.println("Number of families with 4 or more children: " + fourChild); 
